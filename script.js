43af0f7b06c2666d786080fd1ee7dd4168bae29c
Gustavo Alves Miyazaki <gustavo.alves388@gmail.com>
2021-06-01 17:28:18 -0300

    Finalização do projeto e correção do lint

--- script.js
+++ script.js
@@ -1,24 +1,26 @@
 let color = 'black';
-let button = document.getElementById('clear-board');
-let changeSizeButton = document.getElementById("generate-board");
+const button = document.getElementById('clear-board');
+const changeSizeButton = document.getElementById('generate-board');
+const selectedColorClass = 'color selected';
 
-function randomColor(){
-    let red = parseInt(Math.random() * 255);
-    let green = parseInt(Math.random() * 255);
-    let blue = parseInt(Math.random() * 255);
-    let bright = parseInt(Math.random() * 100);
-    return(`rgba(${red},${green},${blue},${bright})`);
-    }
+function randomColor() {
+  const red = parseInt(Math.random() * 255, 10);
+  const green = parseInt(Math.random() * 255, 10);
+  const blue = parseInt(Math.random() * 255, 10);
+  const bright = parseInt((Math.random() * 100), 10);
+  return (`rgba(${red},${green},${blue},${bright})`);
+}
 
 function createColorPallete() {
-  let pallet = document.getElementById('color-palette');
+  const otherColorsClass = 'color';
+  const pallet = document.getElementById('color-palette');
   for (let index = 0; index < 4; index += 1) {
-    let colorDiv = document.createElement('div');
-    let eachColor = ['black', randomColor(), randomColor(), randomColor()];
+    const colorDiv = document.createElement('div');
+    const eachColor = ['black', randomColor(), randomColor(), randomColor()];
     if (eachColor[index] === 'black') {
-      colorDiv.className = 'color selected';
+      colorDiv.className = selectedColorClass;
     } else {
-      colorDiv.className = 'color';
+      colorDiv.className = otherColorsClass;
     }
     colorDiv.style.backgroundColor = (eachColor[index]);
     pallet.appendChild(colorDiv);
@@ -27,17 +29,17 @@ function createColorPallete() {
 createColorPallete();
 
 function pixelFiller(tableSize, line) {
-  let lineColumnFiller = document.getElementsByClassName('lineColumn');
+  const lineColumnFiller = document.getElementsByClassName('lineColumn');
   for (let index = 0; index < tableSize; index += 1) {
-    let pixel = document.createElement('td');
+    const pixel = document.createElement('td');
     pixel.className = 'pixel';
     lineColumnFiller[line].appendChild(pixel);
   }
 }
 function tableMaker(tableSize) {
-  let pixelBoard = document.getElementById('pixel-board');
+  const pixelBoard = document.getElementById('pixel-board');
   for (let index = 0; index < tableSize; index += 1) {
-    let lineColumn = document.createElement('tr');
+    const lineColumn = document.createElement('tr');
     lineColumn.className = 'lineColumn';
     pixelBoard.appendChild(lineColumn);
   }
@@ -48,34 +50,36 @@ function tableMaker(tableSize) {
 tableMaker(5);
 
 function changedPickedColor() {
-  let actualSelectedColor = document.getElementsByClassName('color selected');
+  const actualSelectedColor = document.getElementsByClassName(selectedColorClass);
   color = actualSelectedColor[0].style.backgroundColor;
 }
 
-function selectedColorChanger(button) {
-  let actualSelectedColor = document.getElementsByClassName('color selected');
-  if (button.target.className !== 'color selected') {
+function selectedColorChanger(colorButton) {
+  const colorButtonChanger = colorButton;
+  const actualSelectedColor = document.getElementsByClassName(selectedColorClass);
+  if (colorButtonChanger.target.className !== selectedColorClass) {
     actualSelectedColor[0].className = 'color';
-    button.target.className = 'color selected';
+    colorButtonChanger.target.className = selectedColorClass;
     changedPickedColor();
   }
 }
 
 function colorSelectorButtons() {
-  let colorPalleteContainer = document.getElementById('color-palette');
-  let colorsQuantity = colorPalleteContainer.childElementCount;
+  const colorPalleteContainer = document.getElementById('color-palette');
+  const colorsQuantity = colorPalleteContainer.childElementCount;
   for (let index = 0; index < colorsQuantity; index += 1) {
-    let colorSelect = document.getElementsByClassName('color')[index];
+    const colorSelect = document.getElementsByClassName('color')[index];
     colorSelect.addEventListener('click', selectedColorChanger);
   }
 }
 colorSelectorButtons();
 
 function changeColor(blankPixelToChange) {
-  blankPixelToChange.target.style.backgroundColor = color;
+  const pixelColorChanger = blankPixelToChange;
+  pixelColorChanger.target.style.backgroundColor = color;
 }
 function pixelSelectorButtons() {
-  let blankPixel = document.getElementsByClassName('pixel');
+  const blankPixel = document.getElementsByClassName('pixel');
   for (let index = 0; index < blankPixel.length; index += 1) {
     blankPixel[index].addEventListener('click', changeColor);
   }
@@ -84,7 +88,7 @@ function pixelSelectorButtons() {
 pixelSelectorButtons();
 
 function BoardCleaner() {
-  let pixelsToClear = document.getElementsByClassName('pixel');
+  const pixelsToClear = document.getElementsByClassName('pixel');
   for (let index = 0; index < pixelsToClear.length; index += 1) {
     pixelsToClear[index].style.backgroundColor = 'white';
   }
@@ -92,25 +96,25 @@ function BoardCleaner() {
 
 button.addEventListener('click', BoardCleaner);
 
-function tableSizeInput (inputTextSize){
-    if (inputTextSize == ''){
-        alert('Board inválido!');
-    }
-    if (inputTextSize < 5){
-        return(5);
-    }
-    if (inputTextSize > 50){
-        return(50);
-    }
-    return(inputTextSize);
-    }
-    
-    function clearAndGenerate() {
-        let inputTextSize = document.getElementById("board-size").value;
-        let table =  document.getElementById("pixel-board");
-        table.innerHTML = "";
-        tableMaker(tableSizeInput(inputTextSize));
-        pixelSelectorButtons();
-    }
-    
-    changeSizeButton.addEventListener("click", clearAndGenerate);
+function tableSizeInput(inputTextSize) {
+  if (inputTextSize === '') {
+    alert('Board inválido!');
+  }
+  if (inputTextSize < 5) {
+    return (5);
+  }
+  if (inputTextSize > 50) {
+    return (50);
+  }
+  return (inputTextSize);
+}
+
+function clearAndGenerate() {
+  const inputTextSize = document.getElementById('board-size').value;
+  const table = document.getElementById('pixel-board');
+  table.innerHTML = '';
+  tableMaker(tableSizeInput(inputTextSize));
+  pixelSelectorButtons();
+}
+
+changeSizeButton.addEventListener('click', clearAndGenerate);
