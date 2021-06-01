6e139bd5e94ed6a68df54c5534419201d0f055f1
Gustavo Alves Miyazaki <gustavo.alves388@gmail.com>
2021-06-01 15:53:02 -0300

    Adição de desafios 4, 5 e 6

--- script.js
+++ script.js
@@ -13,3 +13,24 @@ function createColorPallete(){
      }
 }
 createColorPallete();
+
+function pixelFiller(tableSize, line) {
+    let lineColumnFiller = document.getElementsByClassName("lineColumn");
+    for (let index = 0; index < tableSize; index += 1){
+        let pixel = document.createElement("td");
+        pixel.className = "pixel";
+        lineColumnFiller[line].appendChild(pixel);
+    }
+}
+function tableMaker(tableSize) {
+    let pixelBoard = document.getElementById("pixel-board");
+    for (let index = 0; index < tableSize; index += 1){
+        let lineColumn = document.createElement("tr");
+        lineColumn.className = "lineColumn";
+        pixelBoard.appendChild(lineColumn);
+    }
+    for (let line = 0; line < tableSize; line  += 1){
+    pixelFiller(tableSize , line);
+    }
+}
+tableMaker(5);
